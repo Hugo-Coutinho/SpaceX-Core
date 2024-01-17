f8c8f2527fd382aad46448de142ddacca774070a
Hugo Coutinho <>
2024-01-17 17:39:07 +0100

    using HGCore name

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HGCore",
-            targets: ["SpaceX-Core"]),
+            targets: ["HGCore"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SpaceX-Core"),
+            name: "HGCore"),
         .testTarget(
             name: "SpaceX-CoreTests",
-            dependencies: ["SpaceX-Core"]),
+            dependencies: ["HGCore"]),
     ]
 )
--- Tests/SpaceX-CoreTests/SpaceX_CoreTests.swift
+++ Tests/SpaceX-CoreTests/SpaceX_CoreTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import SpaceX_Core
+@testable import HGCore
 
 class DateHelperTests: XCTestCase {
 
