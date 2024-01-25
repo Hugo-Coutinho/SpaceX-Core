246665be752e572d94e3b667a856c28cebae7c09
Hugo Coutinho <>
2024-01-25 21:41:00 +0100

    adding up icons

--- Package.swift
+++ Package.swift
@@ -6,16 +6,17 @@ import PackageDescription
 let package = Package(
     name: "SpaceX-Core",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "HGCore",
             targets: ["HGCore"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "HGCore"),
+            name: "HGCore",
+            resources: [
+                .process("Assets")
+            ]
+        ),
         .testTarget(
             name: "SpaceX-CoreTests",
             dependencies: ["HGCore"]),
